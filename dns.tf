ae2341eba66a388b8219e587801680a64f276ad3
Igor Bo <2994792+31ement@users.noreply.github.com>
2023-12-11 09:46:09 -0500

    Uncomment DNS record

--- dns.tf
+++ dns.tf
@@ -11,20 +11,20 @@ module "dns_zones_dev" {
   tags = var.aws_default_tag
 }
 
-# module "records" {
-#   source  = "terraform-aws-modules/route53/aws//modules/records"
-#   version = "2.10.2"
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "2.10.2"
 
-#   zone_id = module.dns_zones_dev.route53_zone_zone_id["example.com"]
+  zone_id = module.dns_zones_dev.route53_zone_zone_id["example.com"]
 
-#   records = [
-#     {
-#       name    = "www"
-#       type    = "CNAME"
-#       ttl = 300
-#       records = [ module.elb_web.elb_dns_name ]
-#     },
-#   ]
+  records = [
+    {
+      name    = "www"
+      type    = "CNAME"
+      ttl = 300
+      records = [ module.elb_web.elb_dns_name ]
+    },
+  ]
 
-#   depends_on = [module.dns_zones_dev]
-# }
+  depends_on = [module.dns_zones_dev]
+}
